188e91633ce8aa53da15b06c6461a83f13ff2ae7
arturfil <arturfil@hotmail.com>
2021-09-08 16:40:43 -0400

    added form manipulation

--- forms-demo/src/views/AddMovie.js
+++ forms-demo/src/views/AddMovie.js
@@ -8,35 +8,76 @@ const AddMovie = () => {
     title: "",
     director: "",
     IMDBRating: "",
-    hasOscars: null,
+    hasOscars: "",
   });
+
+  const handleChange = (name) => (event) => {
+    setSingleMovie({
+      ...singleMovie,
+      [name]: event.target.value
+    })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setMovies([...movies, singleMovie]);
+    setSingleMovie({
+      title: "",
+      director: "",
+      IMDBRating: "",
+      hasOscars: "",
+    })
+  }
 
   return (
     <>
-      <h2>Add Movie Component</h2>
+      <h2 style={{marginBottom: '40px'}}>Add Movie Component</h2>
+
+      { movies.length > 0 && movies.map((movie, i) => (
+        <div key={i}>
+          <h3>{movie.title}</h3>
+          <p>{movie.director}</p>
+          <p>{movie.IMDBRating}</p>
+          <p>{movie.hasOscars}</p>
+        </div>
+      ))}
+
+      { movies.length == 0 && (
+        <h2>No movies added yet, please add movies</h2>
+      )}
+
 
       <form className="addMovieBox">
         <h3>Add Movie</h3>
         <input
+          onChange={handleChange('title')}
+          value={singleMovie.title}
           className="form-control" 
           type="text" 
           placeholder="title" 
         />
         <input
+          onChange={handleChange('director')}
+          value={singleMovie.director}
           className="form-control" 
           type="text" 
           placeholder="director" 
         />
         <input
+          onChange={handleChange('IMDBRating')}
+          value={singleMovie.IMDBRating}
           className="form-control" 
           type="number" 
           placeholder="IMDBRating" 
         />
         <input
+          onChange={handleChange('hasOscars')}
+          value={singleMovie.hasOscars}
           className="form-control" 
           type="text" 
           placeholder="hasOscars" 
         />
+        <button onClick={handleSubmit} className="btn btn-outline-primary">Create</button>
       </form>
     </>
   );
